c642fcd888bda35328f2206e3bb184d4422a22b3
Kwon Ena <eeena98@naver.com>
2022-04-25 22:52:52 +0900

    Study : subscribe, reducer을 switch문으로 제어

--- src/index.js
+++ src/index.js
@@ -4,43 +4,46 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
+// action : reducer와 소통하는 방식
+// action type을 변수화 시켜 오류를 쉽게 잡을 수 있게 만듦
+// 일반 string 타입은 JS가 오류를 잡지 못함
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 // reducer : data 수정 함수
 // 데이터를 유일하게 수정할 수 있는 곳
 // return 값은 application의 data가 됨
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
 // store : date 저장 공간
-const coutStore = createStore(countModifier);
+const countStore = createStore(countModifier);
 
-// dispatch
-coutStore.dispatch({ type: "ADD" });
+const onChange = () => {
+  number.innerText = countStore.getState();
+};
 
-// vanilla JS로 redux를 사용하지 않고 count의 값 변경
-// let count = 0;
+// subscribe : store의 변화를 감지하면 인자 값으로 준 함수를 실행
+countStore.subscribe(onChange);
 
-// number.innerText = count;
+// dispatch : reducer에 action을 보내고 호출함
+const handleAdd = () => {
+  countStore.dispatch({ type: ADD });
+};
 
-// const updateText = () => {
-//   number.innerText = count;
-// };
+const handleMinus = () => {
+  countStore.dispatch({ type: MINUS });
+};
 
-// const handleAdd = () => {
-//   count = count + 1;
-//   updateText();
-// };
-
-// const handleMinus = () => {
-//   count = count - 1;
-//   updateText();
-// };
-
-// add.addEventListener("click", handleAdd);
-// minus.addEventListener("click", handleMinus);
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
