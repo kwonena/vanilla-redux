b47002705853103b9f91b1adc61ff2c77cbd837d
Kwon Ena <eeena98@naver.com>
2022-04-27 22:49:44 +0900

    Study : ToDo 삭제 기능 구현, 코드 리팩토링

--- src/index.js
+++ src/index.js
@@ -7,15 +7,31 @@ const ul = document.querySelector("ul");
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+// action만 return 해주는 함수
+// 함수를 작은 단위로 쪼개 최적화 할 수 있음
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO,
+    text,
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO,
+    id,
+  };
+};
+
 // store을 수정할 수 있는 방법은 action을 보내는 것뿐
 // state는 single source of truth, read-only
 // state는 mutation(변형) 금지 -> new state objects를 리턴
 const reducer = (state = [], action) => {
   switch (action.type) {
     case ADD_TODO:
-      return [{ text: action.text, id: Date.now() }, ...state];
+      return [...state, { text: action.text, id: Date.now() }];
     case DELETE_TODO:
-      return [];
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
@@ -25,12 +41,16 @@ const store = createStore(reducer);
 
 store.subscribe(() => console.log(store.getState()));
 
-const addToDo = (text) => {
+const dispatchAddToDo = (text) => {
   console.log("store.getState()");
-  store.dispatch({ type: ADD_TODO, text });
+  store.dispatch(addToDo(text));
 };
 
-const deleteToDo = () => {};
+const dispatchDeleteToDo = (e) => {
+  // 삭제 버튼을 누른 부모 노드를 찾아 삭제
+  const id = parseInt(e.target.parenNode.id);
+  store.dispatch(deleteToDo(id));
+};
 
 const paintToDos = () => {
   const toDos = store.getState();
@@ -39,7 +59,7 @@ const paintToDos = () => {
     const li = document.createElement("li");
     const btn = document.createElement("button");
     btn.innerText = "DEL";
-    btn.addEventListener("click", deleteToDo);
+    btn.addEventListener("click", dispatchDeleteToDo);
     li.id = toDo.id;
     li.innerText = toDo.text;
     li.appendChild(btn);
@@ -53,7 +73,7 @@ const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  addToDo(toDo);
+  dispatchAddToDo(toDo);
 };
 
 form.addEventListener("submit", onSubmit);
