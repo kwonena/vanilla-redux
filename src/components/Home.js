8eb6ec8b34d5137aa78e31082d310d14ae0551d1
Kwon Ena <eeena98@naver.com>
2022-05-01 23:46:26 +0900

    Study : mapDispatchToProps

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../routes/store.js";
 
-function Home({ toDos }) {
+function Home({ toDos, addToDo }) {
   const [text, setText] = useState("");
 
   const onChange = (e) => {
@@ -10,6 +11,7 @@ function Home({ toDos }) {
 
   const onSubmit = (e) => {
     e.preventDefault();
+    addToDo(text);
     setText("");
   };
 
@@ -20,7 +22,7 @@ function Home({ toDos }) {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>{JSON.stringify(toDos)}</ul>
     </>
   );
 }
@@ -32,6 +34,14 @@ function mapStateToProps(state) {
   return { toDos: state };
 }
 
+// addToDo 함수가 실행되면 dispatch를 호출
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  };
+}
+
 // connect() : Home으로 보내는 props에 추가할 수 있도록 허용해줌
 // mapStateToProps에서 return한 값이 prop으로 추가됨
-export default connect(mapStateToProps)(Home);
+// state와 dispatch에 관련된 함수를 인자로 가지며 값이 없을 경우 null로 써주는 것이 가능
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
